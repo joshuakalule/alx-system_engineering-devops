d914c959fc4e49f21c23b03f149c47920f553f17
Kalule Joshua <kaljoshkalule@gmail.com>
2024-03-23 21:03:40 +0000

    add dependencies

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
 # install flask from pip3
-package { 'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
+  require  => Package['python3-pip', 'Werkzeug'],
   provider => 'pip3',
 }
