6db91c06a084785872f93c4f0a345b623af92667
Kalule Joshua <kaljoshkalule@gmail.com>
2024-05-07 17:59:28 +0000

    add file 0 and README.md

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Queries the Reddit API."""
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """Get number of subs."""
+    url = 'https://www.reddit.com/api/search_reddit_names.json'
+    user_agent = 'web:BrUtzve5P9cP8IZB8eCasA:v1 (by /u/Fantastic_Payment420)'
+    headers = {
+        'User-Agent': user_agent
+    }
+    params = {
+        'query': subreddit
+    }
+    resp = requests.get(
+        url, headers=headers, allow_redirects=False, params=params
+    )
+    found = resp.json()
+    if subreddit not in found['names']:
+        return 0
+
+    sub_url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    sub_resp = requests.get(
+        sub_url, headers=headers, allow_redirects=False
+    )
+
+    if sub_resp.status_code != 200:
+        return 0
+    data = sub_resp.json()
+    # print(json.dumps(data))
+    subscribers = data['data']['subscribers']
+    return subscribers
