0b031c3d94bb35f6bb45b1679ba02de6bc0e90b0
Kalule Joshua <kaljoshkalule@gmail.com>
2024-05-09 19:10:42 +0000

    correction revert

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""
-Module for number_of_subscribers function
-"""
+"""Queries the Reddit API."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit. If an invalid subreddit is given, the
-    function should return 0.
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    app_name = '0x16-api_advanced'
-    user_name = 'FeelingPsychology300'
-    headers = {'User-Agent': f'{app_name}/0.0.1 by /u/{user_name}'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Get number of subs."""
+    user_agent = 'web:BrUtzve5P9cP8IZB8eCasA:v1 (by /u/Fantastic_Payment420)'
+    headers = {
+        'User-Agent': user_agent
+    }
+    params = {
+        'query': subreddit
+    }
 
-    if response.status_code != 200:
+    sub_url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    sub_resp = requests.get(
+        sub_url, headers=headers, allow_redirects=False
+    )
+
+    if sub_resp.status_code != 200:
         return 0
-
-    return response.json().get('data', {}).get('subscribers', 0)
+    data = sub_resp.json()
+    subscribers = data.get('data', {}).get('subscribers', 0)
+    return subscribers
